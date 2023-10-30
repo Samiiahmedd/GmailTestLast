c75a7a2dd9c2a5766665757bf5bbfaed30ca35e8
Samiiahmed <eldon.smsm135@gmail.com>
2023-10-30 19:29:21 +0200

    confirm

--- GmailTestLast/ViewController.swift
+++ GmailTestLast/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import GoogleSignIn
+//
 
 class ViewController: UIViewController {
+    let signInConfing = GIDConfiguration.init(clientID: "    67522254591-jjjt1vjtu5dnrjeindm5hmkobf8n7m1a.apps.googleusercontent.com")
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func btnLogin(_ sender: Any) {
+          GIDSignIn.sharedInstance.signIn(withPresenting: self) { signInResult, error in
+            guard error == nil else { return }
+                  guard let signInResult = signInResult else { return }
 
-}
+                  let user = signInResult.user
+
+                  let emailAddress = user.profile?.email
+
+            // If sign in succeeded, display the app's main content View.
+          }
+        }
+    }
+    
+
 
